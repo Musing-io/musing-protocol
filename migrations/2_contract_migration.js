2da21ef63eb3f98349c8c66c8dbc72f72c8dcb1e
Mervin <mervin@musing.io>
2022-02-17 11:25:03 +0800

    economy token

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,9 +1,11 @@
 const MusingToken = artifacts.require("MusingToken");
 const EndorseUser = artifacts.require("EndorseUser");
 const UserPost = artifacts.require("UserPost");
+const EconomyToken = artifacts.require("EconomyToken");
 
 module.exports = async function (deployer) {
   await deployer.deploy(MusingToken, "2000000000000000000000000");
+  await deployer.deploy(EconomyToken, 'ECONOMY', 'ECON', "2000000000000000000000000");
   await deployer.deploy(EndorseUser, MusingToken.address);
   await deployer.deploy(UserPost, MusingToken.address);
 };
